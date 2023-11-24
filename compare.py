81f2d88a6b495f00376f1bcb1df6484e87636c84
Shweta <shweta.malav@gmail.com>
2023-11-25 00:26:57 +0100

    New file is created to learn conditions in python.

---
+++ compare.py
@@ -0,0 +1,39 @@
+x = int(input('What is x?'))
+y = int(input('What is y??'))
+
+
+
+'''
+if x > y:
+    print('x is greater than y.')
+
+if x < y:
+    print('x is less than y.', x , y)
+
+if x == y:
+    print('x is equal to y.', x , y)
+    
+'''
+'''
+if x > y:
+    print('x is greater than y.')
+
+elif x < y:
+    print('x is less than y.', x , y)
+
+elif x == y:
+    print('x is equal to y.', x , y)
+
+'''
+
+
+
+
+if x > y:
+    print('x is greater than y.')
+
+elif x < y:
+    print('x is less than y.', x , y)
+
+else:
+    print('x is equal to y.', x , y)
