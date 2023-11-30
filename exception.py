c4d2309359b2fab03ee097688006c56c32d4df90
Shweta <shweta.malav@gmail.com>
2023-11-30 02:05:13 +0100

    Python is in learning.

--- exception.py
+++ exception.py
@@ -7,7 +7,8 @@ except ValueError:
 '''
 
 
-
+#print statement is moved
+'''
 try:
     x = int(input('What is X??'))
 
@@ -15,3 +16,54 @@ except ValueError:
     print('Invaliddd Input')
 else:
     print(f'x is {x}')
+'''
+
+
+#using while loop - on invalid input loop will start again
+'''
+while True:
+    try:
+        x = int(input('What is X??'))
+    except ValueError:
+        print('Invaliddd Input')
+    else:
+        break
+        
+
+print(f'x is {x}')
+'''
+
+
+
+#little less codes
+'''
+while True:
+    try:
+        x = int(input('What is X??'))
+        break
+    except ValueError:
+        print('Invaliddd Input')     
+        
+
+print(f'x is {x}')
+'''
+
+
+
+#main function
+def main():
+    x =  get_int()
+    print(f'x is {x}')
+
+
+def get_int():
+    while True:
+        try:
+            x = int(input('What is X??'))
+            break
+        except ValueError:
+            print('Invaliddd Input')  
+    return x  
+  
+
+main()
