d8712bdeaa001c9d97d361ce9681666550db4d35
Shweta <shweta.malav@gmail.com>
2023-12-06 04:27:07 +0100

    Python is in learning.

---
+++ library.py
@@ -0,0 +1,13 @@
+'''
+import random
+
+coin = random.choice(['Head', 'Tail'])
+
+print(coin)
+'''
+
+#Only needed function is imported
+from random import choice
+coin =  choice(['Head', 'Tail'])
+print(coin)
+
