260955b8f4190ddac54ccd70bf73d18c3f8dfb96
Shweta <shweta.malav@gmail.com>
2023-12-06 11:22:14 +0100

    Python

--- library.py
+++ library.py
@@ -7,7 +7,25 @@ print(coin)
 '''
 
 #Only needed function is imported
+'''
 from random import choice
-coin =  choice(['Head', 'Tail'])
+options = ['Head', 'Tail']
+coin =  choice(options)
 print(coin)
+'''
 
+#print random number
+'''
+import random
+printRandom = random.randint(0, 90)
+print(printRandom)
+'''
+
+
+#shuffle list of something
+#shuffle does the a new shuffled list
+import random
+cards = ['King', 'Queen', 'Jack', '10', '9']
+random.shuffle(cards)
+for card in cards:
+    print(card)
